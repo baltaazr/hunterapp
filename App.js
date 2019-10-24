78c8ca8f6ae17cf17971231a4b3c22aa01e77077
ian13456 <21ianh1@tas.tw>
2019-10-24 09:56:38 +0800

    feat: eslint

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen'
+import ListScreen from './src/screens/ListScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    List: ListScreen
   },
-});
+  {
+    initialRouteName: 'List',
+    defaultNavigationOptions: {
+      title: 'App'
+    }
+  }
+)
+
+export default createAppContainer(navigator)
---
+++ src/screens/ListScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default () => (
+  <View>
+    <Text>LIST SCREEN</Text>
+  </View>
+)
+
+// const styles = StyleSheet.create({})
