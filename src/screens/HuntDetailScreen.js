d95ee6fa491c0edeaf45e66f3b324d0a9f252922
mrprokoala <zunigaruizb@gmail.com>
2019-10-30 01:00:30 +0800

    feat: added to huntlistscreen and huntdetailscreen

--- src/screens/HuntDetailScreen.js
+++ src/screens/HuntDetailScreen.js
@@ -1,6 +1,40 @@
-import React from 'react'
-import { Text } from 'react-native'
+/* eslint-disable no-underscore-dangle */
+import { HuntContext } from '../context'
 
-const HuntDetailScreen = () => <Text>HuntDetailScreen</Text>
+import React, { useContext } from 'react';
+import { StyleSheet, Text, Image, FlatList } from 'react-native';
+import { FORM_ITEMS } from 'config'
+
+const styles = StyleSheet.create({
+  question: {
+    fontWeight: 'bold'
+  }
+})
+
+const HuntDetailScreen = ({ navigation }) => {
+  const { state } = useContext(HuntContext)
+
+  const _id = navigation.getParam('_id')
+
+  const hunt = state.find(t => t._id === _id)
+
+  const base64Icon = `data:image/png;base64,${hunt.picture}`
+
+  return (
+    <>
+      {/* <Image source={{ uri: base64Icon }} /> */}
+      <FlatList
+        data={hunt.formInfo}
+        renderItem={({ item, index }) => (
+          <>
+            <Text style={styles.question}>{`${FORM_ITEMS[index].question}: `}</Text>
+            <Text>{item}</Text>
+          </>
+        )}
+        keyExtractor={item => item.question}
+      />
+    </>
+  )
+}
 
 export default HuntDetailScreen
--- src/screens/HuntListScreen.js
+++ src/screens/HuntListScreen.js
@@ -1,6 +1,33 @@
-import React from 'react'
-import { Text } from 'react-native'
+/* eslint-disable no-underscore-dangle */
+import { HuntContext } from '../context'
 
-const HuntListScreen = () => <Text>HuntListScreen</Text>
+import React, { useContext } from 'react'
+import { FlatList, TouchableOpacity } from 'react-native'
+import { NavigationEvents } from 'react-navigation';
+import { ListItem } from 'react-native-elements';
+
+const HuntListScreen = ({ navigation }) => {
+  const { state, fetchHunts } = useContext(HuntContext)
+  return (
+    <>
+      <NavigationEvents onWillFocus={fetchHunts} />
+      <FlatList
+        data={state}
+        keyExtractor={item => item._id}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate('HuntDetail', { _id: item._id })}
+          >
+            <ListItem chevron title={item.date.toString()} />
+          </TouchableOpacity>
+        )}
+      />
+    </>
+  )
+}
+
+HuntListScreen.navigationOptions = {
+  title: 'Hunts'
+}
 
 export default HuntListScreen
