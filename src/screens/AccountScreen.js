79e579206c44aedabccf8e6a4f6d73baeb6f975e
dvfcfbgv <21ianh1@tas.tw>
2019-10-29 20:50:55 +0800

    feat: signout button

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,6 +1,28 @@
-import React from 'react'
-import { Text } from 'react-native'
+import Spacer from '../components/Spacer'
+import { AuthContext } from '../context'
 
-const AccountScreen = () => <Text>AccountScreen</Text>
+import React, { useContext } from 'react'
+import { Text } from 'react-native'
+import { Button } from 'react-native-elements'
+import { SafeAreaView } from 'react-navigation'
+import { FontAwesome } from '@expo/vector-icons'
+
+const AccountScreen = () => {
+  const { signout } = useContext(AuthContext)
+
+  return (
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <Text style={{ fontSize: 48 }}>AccountScreen</Text>
+      <Spacer>
+        <Button title="Sign Out" onPress={signout} />
+      </Spacer>
+    </SafeAreaView>
+  )
+}
+
+AccountScreen.navigationOptions = {
+  title: 'Account',
+  tabBarIcon: <FontAwesome name="gear" size={20} />
+}
 
 export default AccountScreen
