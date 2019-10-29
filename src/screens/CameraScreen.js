0cd5015c840c029f377c247c2c0a8b6d2ba1075c
mrprokoala <zunigaruizb@gmail.com>
2019-10-29 21:29:38 +0800

    feat: camera capture

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,21 +1,22 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, Button, Image } from 'react-native'
 import * as Permissions from 'expo-permissions'
 import { Camera } from 'expo-camera'
+import { NavigationEvents } from 'react-navigation'
 
 const styles = StyleSheet.create({
   container: { flex: 1 },
   camera: { flex: 1 },
-  cameraView: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    flexDirection: 'row'
-  }
+  picture: { flex: 1 }
 })
+
+let camera
 
 const CameraScreen = () => {
   const [perm, setPerm] = useState(null)
+  const [focus, setFocus] = useState(true)
+  const [picture, setPicture] = useState(null)
 
   useEffect(() => {
     const askCameraPermission = async () => {
@@ -30,11 +31,41 @@ const CameraScreen = () => {
     }
   }, [])
 
+  const snap = async () => {
+    if (camera) {
+      const pic = await camera.takePictureAsync()
+      setPicture(pic)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      {perm === 'granted' ? (
-        <Camera style={styles.camera} type={Camera.Constants.Type.back}>
-          <View style={styles.cameraView} />
+      <NavigationEvents
+        onWillBlur={() => {
+          setFocus(false)
+        }}
+        onWillFocus={() => {
+          setFocus(true)
+        }}
+      />
+      {picture ? (
+        <>
+          <Image source={{ uri: picture.uri }} style={styles.picture} />
+          <Button title="Proceed" />
+        </>
+      ) : perm === 'granted' && focus ? (
+        <Camera
+          style={styles.camera}
+          ref={ref => {
+            camera = ref
+          }}
+        >
+          <Button
+            title="Take Picture"
+            onPress={() => {
+              snap()
+            }}
+          />
         </Camera>
       ) : perm === 'denied' ? (
         <Text>Please grant camera access</Text>
