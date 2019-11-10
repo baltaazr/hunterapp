721d686812d8a4cce41de5b5a501859b172e82f4
mrprokoala <zunigaruizb@gmail.com>
2019-11-10 18:11:07 +0800

    feat: try catch to catch weather api errors

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -156,32 +156,47 @@ const CameraScreen = ({ navigation }) => {
 
   const snap = async () => {
     const pic = await camera.takePictureAsync({ base64: true })
-    const location = await Location.getCurrentPositionAsync({})
-    const { data } = await weatherApi.get(
-      '/locations/v1/cities/geoposition/search',
-      {
-        params: {
-          apikey: weatherApiKey,
-          q: `${location.coords.latitude},${location.coords.longitude}`
+    const location = await Location.getCurrentPositionAsync()
+    try {
+      const { data } = await weatherApi.get(
+        '/locations/v1/cities/geoposition/search',
+        {
+          params: {
+            apikey: weatherApiKey,
+            q: `${location.coords.latitude},${location.coords.longitude}`
+          }
         }
-      }
-    )
-    const weather = await weatherApi.get(`/currentconditions/v1/${data.Key}`, {
-      params: {
-        apikey: weatherApiKey,
-        details: true
-      }
-    })
-    const weatherObj = weather.data[0]
-    setPictureData({
-      picture: pic,
-      location,
-      weather: {
-        temperature: weatherObj.Temperature.Metric.Value,
-        text: weatherObj.WeatherText,
-        humidity: weatherObj.RelativeHumidity
-      }
-    })
+      )
+      const weather = await weatherApi.get(
+        `/currentconditions/v1/${data.Key}`,
+        {
+          params: {
+            apikey: weatherApiKey,
+            details: true
+          }
+        }
+      )
+      const weatherObj = weather.data[0]
+      setPictureData({
+        picture: pic,
+        location,
+        weather: {
+          temperature: weatherObj.Temperature.Metric.Value,
+          text: weatherObj.WeatherText,
+          humidity: weatherObj.RelativeHumidity
+        }
+      })
+    } catch (e) {
+      setPictureData({
+        picture: pic,
+        location,
+        weather: {
+          temperature: -1,
+          text: 'UNABLE TO GET WEATHER DATA',
+          humidity: -1
+        }
+      })
+    }
   }
 
   return (
